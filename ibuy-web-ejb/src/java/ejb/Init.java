8ae0f87b5f2900d0ae2955c3cd66a834a4a98c87
misaina08 <misa.andrianary@gmail.com>
2017-08-21 15:28:01 +0300

    init

--- ibuy-web-ejb/src/java/ejb/Init.java
+++ ibuy-web-ejb/src/java/ejb/Init.java
@@ -6,6 +6,9 @@
 package ejb;
 
 import com.mongodb.MongoClient;
+import entity.Client;
+import entity.SuperAdmin;
+import java.util.Date;
 import javax.annotation.PostConstruct;
 import javax.annotation.sql.DataSourceDefinition;
 import javax.ejb.Singleton;
@@ -33,7 +36,7 @@ public class Init {
 
     @PersistenceContext(unitName = "ibuy-web-ejbPU")
     private EntityManager em;
-    
+
     @PostConstruct
     public void init() {
         MongoClient mongoClient = null;
@@ -52,6 +55,19 @@ public class Init {
 //                    System.out.println("\t + Collection: " + colName);
 //                }
 //            }
+//            Client c = new Client();
+//            c.setNom("Premier");
+//            c.setPrenom("Client");
+//            c.setDateInscription(new Date());
+//            c.setLogin("client1");
+//            c.setMdp("client1");
+//            c.setEmail("c@gmail.com");
+//            em.persist(c);
+//
+//            SuperAdmin superAdmin = new SuperAdmin();
+//            superAdmin.setMdp("super");
+//            superAdmin.setLogin("super");
+//            em.persist(superAdmin);
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
